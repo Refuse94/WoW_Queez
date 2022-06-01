a97eba283066fb85090c258e72fd2644d4246ecb
Refuse94 <simko.tomas94@gmail.com>
2022-06-01 16:57:55 +0200

    pridanie data atributu  a následného výpisu do console

--- script.js
+++ script.js
@@ -2,24 +2,34 @@
 
 console.log('počujeme sa? ');
 
-var quests = $('.container').find('div');
+var quests = $('.quests').find('div');
 quests.addClass('hide');
 quests.eq(0).removeClass('hide');
-var selected = $('.container').find('.selected');
+var selected = $('.quests').find('.selected');
+var li = $('.quests').find('li');
 
 
 quests.find('li').on('click', function(){
+    schovaj();
+    console.log(li.attr('data-index-number'));
+   
+})
+
+function schovaj (){
     selected.addClass('hide');
     var nextSelected = selected.next();
     selected = nextSelected;
     selected.addClass('selected').siblings().removeClass('selected');
     selected.removeClass('hide');
-})
+}
 
 
 
-
-
+// const events = document.querySelector('.quests');
+// events.forEach(e => {
+//     console.log(e);
+    
+// })
 
 
 
