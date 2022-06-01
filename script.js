ffff53aa18c713f8e3a14d65dece4e944ca5a09b
Refuse94 <simko.tomas94@gmail.com>
2022-06-01 17:55:47 +0200

    pridanie timeru ktorý nefunguje xD

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ var li = $('.quests').find('li');
 var liScore =li.attr('data-index-number');
 
 
+
+
 li.on('click', function(){
     schovaj();
     console.log(li.attr('data-index-number')); // Nerozumiem, prečo keď kliknem na iný "li" element, tak vypisuje stále hodnotu prvého.
@@ -47,9 +49,32 @@ function score(a, b) {
   console.log(dataScore);
 
 
+  var amNew = $('<div/>').html('SCORE');      
+      amNew.appendTo('footer');
+
+// var intiger = 10;
+// intiger.appendTo('footer');
 
 
 
+
+
+
+
+const startingMinuites = 10;
+let time = startingMinuites *60;
+const countdownEl = document.getElementById('countdown');
+setInterval(updateCountdown, 1000);
+
+function updateCountdown(){
+    const minutes =Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML= '${minutes}: ${seconds}';
+    time--;
+}
 
 
 
