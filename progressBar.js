8b4bd2cbacf7d3d1775fd3240b520cc452f5ca7a
Refuse94 <simko.tomas94@gmail.com>
2022-06-03 10:45:23 +0200

    Add progress_bar

---
+++ progressBar.js
@@ -0,0 +1,19 @@
+function updateProgressBar(progressBar, value) {
+    value = Math.round(value);
+    progressBar.querySelector(".progress__fill").style.width = `${fill}%`;
+    progressBar.querySelector(".progress__text").textContent = `${value}/10`;
+  }
+  
+  const myProgressBar = document.querySelector(".progress");
+  
+  /* Example */
+//   updateProgressBar(myProgressBar, 30);
+
+  var li = $('.quests').find('li');
+  var value= 1;
+  var fill = 10;
+ 
+
+  li.on('click', function(){
+    updateProgressBar(myProgressBar, value++, fill++);
+})
