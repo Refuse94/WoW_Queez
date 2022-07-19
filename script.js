27b70c119e08cfb0bca92c60bdbb5e3cb425afa3
Refuse94 <simko.tomas94@gmail.com>
2022-07-19 17:13:42 +0200

    dokonceny timer

--- script.js
+++ script.js
@@ -1,77 +1,112 @@
-(function($){
+(function ($) {
 
-var quests = $('.quests').find('div');
-quests.addClass('hide');
-quests.eq(0).removeClass('hide');
+    var quests = $('.quests').find('div');
+    quests.addClass('hide');
+    quests.eq(0).removeClass('hide');
 
-var selected = $('.quests').find('.selected');
-var li = $('.quests').find('li');
+    var selected = $('.quests').find('.selected');
+    var li = $('.quests').find('li');
 
-var score = $('#score');
-var totalScore = 0;
-
-
-
-
-li.on('click', function(){
-    schovaj();
-    totalScore += $(this).data('value')*1;
-    score.text('Score:'+ ' ' + totalScore);
-    console.log($(this).data('score'));
-    clearInterval(time_out);
-    timer();
-
-   
-})
-
-function schovaj (){
-    selected.addClass('hide');
-    var nextSelected = selected.next();
-    selected = nextSelected;
-    selected.addClass('selected').siblings().removeClass('selected');
-    selected.removeClass('hide');
-}
-// =================== Asi iba po tialto to funguje xD ====================
-
-
-// ============== toto malo tiež vytiahnuť data atribut ale nejako nefungoval forEach =============
-
-// const events = document.querySelector('.quests');
-// events.forEach(e => {
-//     console.log(e);
-    
-// })
-
-// ============================  Skusanie si math operácie a ako to vlastne funguje, / snaha o prirátanie data atributu k vytvorenému atributu v premennej ==============
-
-
-  
-
-
-// =====================================Vytvorenie SCORE DIVU ktorý sa zobrazí na konci , chcem ho potom naštýlovať na poslednej stránke  ktorá sa zobrazí a nejak naň naviazať ten
-// =======================================      neexistujúci prepočet čo možno niekedy vznikne       ===================================================================
+    var score = $('#score');
+    var totalScore = 0;
 
 
 
 
-// var intiger = 10;
-// intiger.appendTo('footer');
+    li.on('click', function () {
+        schovaj();
+        clearInterval(timer_id);
+        timer();
+        time_limit = 5;
+
+
+        if (time_out == true) {
+            ukaz()
+        } else {
+            console.log("totalscore", totalScore);
+            totalScore += $(this).data('value') * 1;
+
+            console.log("totalscore", totalScore);
+            score.text('Score:' + ' ' + totalScore);
+        }
 
 
 
 
-//========= keď mi ešte nefungoval slider na otázky, tak som sa snažil nejako získať cez html idčko každej otázky a slidovať to cez to idčko , / = nedokázal som získať idčko =====
 
 
-// var id = container.find('id');
+    })
 
-// console.log(id);
+    function schovaj() {
+        selected.addClass('hide');
+        var nextSelected = selected.next();
+        selected = nextSelected;
+        selected.addClass('selected').siblings().removeClass('selected');
+        selected.removeClass('hide');
 
-// container.find('li').on('click', function(){
-//     container.addClass('hide');
-//     var nextId = id.next();
-    
-// })
+    }
+
+    function ukaz() {
+        $('#timer').html("").show();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // =================== Asi iba po tialto to funguje xD ====================
+
+
+    // ============== toto malo tiež vytiahnuť data atribut ale nejako nefungoval forEach =============
+
+    // const events = document.querySelector('.quests');
+    // events.forEach(e => {
+    //     console.log(e);
+
+    // })
+
+    // ============================  Skusanie si math operácie a ako to vlastne funguje, / snaha o prirátanie data atributu k vytvorenému atributu v premennej ==============
+
+
+
+
+
+    // =====================================Vytvorenie SCORE DIVU ktorý sa zobrazí na konci , chcem ho potom naštýlovať na poslednej stránke  ktorá sa zobrazí a nejak naň naviazať ten
+    // =======================================      neexistujúci prepočet čo možno niekedy vznikne       ===================================================================
+
+
+
+
+    // var intiger = 10;
+    // intiger.appendTo('footer');
+
+
+
+
+    //========= keď mi ešte nefungoval slider na otázky, tak som sa snažil nejako získať cez html idčko každej otázky a slidovať to cez to idčko , / = nedokázal som získať idčko =====
+
+
+    // var id = container.find('id');
+
+    // console.log(id);
+
+    // container.find('li').on('click', function(){
+    //     container.addClass('hide');
+    //     var nextId = id.next();
+
+    // })
 
 
 
--- timer.js
+++ timer.js
@@ -1,30 +1,33 @@
-var time_out;
+var time_out=false;
+var timer_id;
 
 
-function timer(){
+function timer() {
   var time_limit = 5;
 
- time_out = setInterval(() => {
-  
-    if(time_limit == 0) {
-      
-      $('#timer').html('Time Over');
-      schovaj();
-          
+
+  timer_id = setInterval(() => {
+    if (time_limit == 0) {
+      time_out = true
+
+      $('#timer').html('Time Over').hide(1000);
+
+
+
+
     } else {
-      
-      if(time_limit < 10) {
+      time_out = false
+      if (time_limit < 10) {
         time_limit = 0 + '' + time_limit;
       }
-      
+
       $('#timer').html('00:' + time_limit);
-      
+
       time_limit -= 1;
-      
+
     }
-  
   }, 1000);
 }
 
+timer();
 
-timer()
